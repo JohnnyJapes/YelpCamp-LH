cf9e3fcbff733b002196d86a0daf2579a37e7a30
Luke Hanrahan <lhanrahan2@gmail.com>
2021-08-09 09:54:28 -0400

    Arrays for fake names to seed DB

---
+++ seeds/seedHelpers.js
@@ -0,0 +1,40 @@
+//Simple setup to create fake names for test campgrounds
+
+const descriptor = [
+    'Forest',
+    'Ancient',
+    'Petrified',
+    'Abandoned',
+    'Crystal',
+    'Roaring',
+    'Bullfrong',
+    'Maple',
+    'Briarwood',
+    'Redwood',
+    'Misty',
+    'Ocean',
+    'Sky',
+    'Dusty',
+    'Silent',
+    'Jellyfish',
+    'Dolphin'
+]
+
+const places = [
+    'Village',
+    'Canyon',
+    'Pond',
+    'Camp',
+    'Creek',
+    'Bay',
+    'Sands',
+    'Lake',
+    'Hollow',
+    'Flats',
+    'Spring',
+    'Cliffs',
+    'Pond',
+    'Grove',
+    'Woods',
+    'Cove'
+]
