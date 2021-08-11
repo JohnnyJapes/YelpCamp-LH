7d28d36dd45ee9cdb5d617418fb728fb56f3d2fc
Luke Hanrahan <lhanrahan2@gmail.com>
2021-08-11 12:01:26 -0400

    Seed now generates description, price, and image

--- seeds/index.js
+++ seeds/index.js
@@ -35,10 +35,14 @@ const seedDB = async () => {
     //loop to seed fake data
     for (let i = 0; i < 60; i++) {
         const random1000 = Math.floor(Math.random()*1000 +1);
+        const random100 = parseFloat((Math.random()*99 +1).toFixed(2));
         const temp = new Campground({
             title:`${descriptors[Math.floor(Math.random()*descriptors.length)]} ${
                 places[Math.floor(Math.random()*places.length)]}`,
-            location:`${cities[random1000].city}, ${cities[random1000].state}`});
+            location:`${cities[random1000].city}, ${cities[random1000].state}`, 
+            image: "https://source.unsplash.com/collection/483251",
+            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, ullam? Sapiente temporibus ea debitis enim autem blanditiis at, beatae neque rem distinctio harum adipisci placeat facere repellendus mollitia eos. Nobis?",
+            price: random100});
         await temp.save();    
     }
 };
