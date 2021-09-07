668b11a430ff809908e37945ffc3eb3187abc1d8
Luke Hanrahan <lhanrahan2@gmail.com>
2021-09-07 12:32:48 -0400

    MVC basic implementation start, functions moved

---
+++ controllers/campgrounds.js
@@ -0,0 +1,89 @@
+const ExpressError = require('../utils/expressError');
+const catchAsync = require('../utils/catchAsync')
+//create model using campground schema
+const Campground = require('../models/campground');
+
+//Render Campground index page
+module.exports.renderIndex = catchAsync(async (req, res, next) => {
+    const campgrounds = await Campground.find({}).exec()
+    .then((campgrounds)=> {
+        res.render('campgrounds/index', {campgrounds});
+    })
+});
+//render Campground details HTML page
+module.exports.renderDetails = catchAsync(async (req, res, next) => {
+    console.log("Show Route");
+    // try{
+    const {id} = req.params;
+    console.log(id);
+    
+    const campground = await Campground.findById(id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author')
+    .then(function(campground){
+        if(!campground) throw new ExpressError("Page not Found", 404);
+        else{
+           // console.log(campground)
+            res.render('campgrounds/show', {campground});
+        }
+        
+    })
+});
+//render New campground HTML page
+module.exports.renderNew = (req, res) => {
+    res.render("campgrounds/new");
+};
+//render Campground edit HTML page
+module.exports.renderEdit = catchAsync(async (req, res, next) => {
+    const {id} = req.params;
+    const campground = await Campground.findById(id).exec()
+    .then(function(campground){
+        if(!campground) throw new ExpressError("Page not Found", 404);
+        else
+        res.render('campgrounds/edit', {campground});
+    })
+});
+//create route - Campground, creates new campground document and inserts it into the database
+module.exports.createNew = catchAsync(async (req, res, next) => {
+    // if(!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
+    
+    req.body.campground.author = req.user._id;
+     await Campground.insertMany(req.body.campground)
+    .then((camp)=>{
+        console.log(camp);
+        
+        req.flash('success', 'Successfully made a new campground'); //appends messages to top of redirect page
+        res.redirect(`/campgrounds/${camp[0]._id}`)
+    })
+});
+//update route, updates existing campground document based on id
+module.exports.update = catchAsync(async (req, res, next) => {
+    const {id} = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, req.body.campground, {new: true, runValidators: true} ).exec()
+    .then((campground)=>{
+        if(!campground) throw new ExpressError("Invalid Campground Data", 400);
+        else{
+            //appends messages to top of redirect page
+            req.flash('success', 'Successfully updated campground');
+            res.redirect(`/campgrounds/${campground._id}`)
+        }
+    })
+});
+//delete route, deletes existing campground document based off id
+module.exports.delete = catchAsync(async (req, res,next) => {
+    console.log("delete");
+    const {id} = req.params;
+    console.log(id);
+    const campground = await Campground.findByIdAndDelete(id).exec()
+    .then((campground)=>{
+        if(!campground) throw new ExpressError("Page not Found", 404);
+        else{
+            //appends messages to top of redirect page
+            req.flash('success', 'Successfully deleted campground');
+            res.redirect(`/campgrounds`)
+        }
+    })   
+});
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,105 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const ExpressError = require('../utils/expressError');
-const catchAsync = require('../utils/catchAsync')
+const campgrounds = require('../controllers/campgrounds')
 const {isLoggedIn, isAuthor, validateCampground} = require('../middleware')
-//create model using campground schema
-const Campground = require('../models/campground');
 
 //new campground page
-router.get('/new',isLoggedIn, (req, res) => {
-    res.render("campgrounds/new");
-});
+router.get('/new',isLoggedIn, campgrounds.renderNew);
 //index route
-router.get('/', catchAsync(async (req, res, next) => {
-    const campgrounds = await Campground.find({}).exec()
-    .then((campgrounds)=> {
-        res.render('campgrounds/index', {campgrounds});
-    })
-}));
+router.get('/', campgrounds.renderIndex);
 
 
 //Show/Details route
-router.get('/:id', catchAsync(async (req, res, next) => {
-    console.log("Show Route");
-    // try{
-    const {id} = req.params;
-    console.log(id);
-    
-    const campground = await Campground.findById(id).populate({
-        path: 'reviews',
-        populate: {
-            path: 'author'
-        }
-    }).populate('author')
-    .then(function(campground){
-        if(!campground) throw new ExpressError("Page not Found", 404);
-        else{
-           // console.log(campground)
-            res.render('campgrounds/show', {campground});
-        }
-        
-    })
-})
-);
+router.get('/:id', campgrounds.renderDetails);
+
 //edit route
-router.get('/:id/edit',isLoggedIn, isAuthor, catchAsync(async (req, res, next) => {
-    const {id} = req.params;
-    const campground = await Campground.findById(id).exec()
-    .then(function(campground){
-        if(!campground) throw new ExpressError("Page not Found", 404);
-        else
-        res.render('campgrounds/edit', {campground});
-    })
-}));
+router.get('/:id/edit',isLoggedIn, isAuthor, campgrounds.renderEdit);
 
 //create route - Campground
-router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
-    // if(!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
-    
-    req.body.campground.author = req.user._id;
-     await Campground.insertMany(req.body.campground)
-    .then((camp)=>{
-        console.log(camp);
-        
-        req.flash('success', 'Successfully made a new campground'); //appends messages to top of redirect page
-        res.redirect(`/campgrounds/${camp[0]._id}`)
-    })
-}));
+router.post('/', isLoggedIn, validateCampground, campgrounds.createNew);
+
 //Update route
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res, next) => {
-    //if(!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
-    const {id} = req.params;
-    //const {title, price, description, city, state, image} = req.body.campground;
-    //const location = city+", "+state;
-    //const location = {city, state};
-    const campground = await Campground.findByIdAndUpdate(id, req.body.campground, {new: true, runValidators: true} ).exec()
-    .then((campground)=>{
-        if(!campground) throw new ExpressError("Invalid Campground Data", 400);
-        else{
-            //appends messages to top of redirect page
-            req.flash('success', 'Successfully updated campground');
-            res.redirect(`/campgrounds/${campground._id}`)
-        }
-    })
-}));
+router.put('/:id', isLoggedIn, isAuthor, validateCampground, campgrounds.update);
+
 //delete - Campground
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res,next) => {
-    console.log("delete");
-    const {id} = req.params;
-    console.log(id);
-    const campground = await Campground.findByIdAndDelete(id).exec()
-    .then((campground)=>{
-        if(!campground) throw new ExpressError("Page not Found", 404);
-        else{
-            //appends messages to top of redirect page
-            req.flash('success', 'Successfully deleted campground');
-            res.redirect(`/campgrounds`)
-        }
-    })   
-}));
-
-
+router.delete('/:id', isLoggedIn, isAuthor, campgrounds.delete);
 
 
 module.exports = router;
