35c58c62497905ce0b1ca6733dbcd901a3ca3d63
Luke Hanrahan <lhanrahan2@gmail.com>
2021-09-16 10:04:11 -0400

    comment

--- index.js
+++ index.js
@@ -108,6 +108,7 @@ app.use((req, res, next) => {
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
+    //local for cloudinary. Primarily for image display
     res.locals.cloudinary = cloudinary;
     next();
 })
