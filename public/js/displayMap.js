1da76a40f7cce7be83ef38a8993cdf7b6bdd69fa
Luke Hanrahan <lhanrahan2@gmail.com>
2021-09-17 12:30:57 -0400

    add basic marker for map pass coordinates

--- public/js/displayMap.js
+++ public/js/displayMap.js
@@ -3,6 +3,9 @@ mapboxgl.accessToken = mbxToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
+    center: ( coordinates || [-74.5, 40]), // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
+const mark = new mapboxgl.Marker()
+    .setLngLat(coordinates)
+    .addTo(map)
